a3796c8d797953391b768db39a8c338ad97283a4
HongQuan78 <vhquan313@gmail.com>
2025-11-22 16:11:00 +0700

    fix: enhance file validation in file upload

--- Chatty.BE.API/Controllers/FilesController.cs
+++ Chatty.BE.API/Controllers/FilesController.cs
@@ -1,4 +1,5 @@
 using Chatty.BE.API.Contracts.Files;
+using Chatty.BE.Application.Common.Helpers;
 using Chatty.BE.Application.Interfaces.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -18,6 +19,8 @@ namespace Chatty.BE.API.Controllers
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status413PayloadTooLarge)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [RequestSizeLimit(10 * 1024 * 1024)]
+        [RequestFormLimits(MultipartBodyLengthLimit = 10 * 1024 * 1024)]
         public async Task<IActionResult> UploadFile(
             [FromForm] UploadFileForm request,
             CancellationToken ct = default
@@ -26,6 +29,17 @@ namespace Chatty.BE.API.Controllers
             if (request.File is null || request.File.Length == 0)
             {
                 return BadRequest("No file uploaded.");
+            }
+
+            const long maxBytes = 10 * 1024 * 1024; // 10 MB
+            if (request.File.Length > maxBytes)
+            {
+                return BadRequest("File is too large. Max 10 MB.");
+            }
+
+            if (!FileValidationService.IsValidImageFileExtension(request.File.FileName))
+            {
+                return BadRequest("Unsupported file type.");
             }
 
             using var stream = request.File.OpenReadStream();
